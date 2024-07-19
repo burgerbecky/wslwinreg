d8ab619eaa6bb2fa34f4050d338f2713e50b46a5
Rebecca Heineman <becky@burgerbecky.com>
2024-07-18 23:46:49 -0500

    Added python 3.12 to tox

--- setup.py
+++ setup.py
@@ -76,7 +76,8 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
-    "Programming Language :: Python :: 3.11"
+    "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12"
 ]
 
 # Extra files to include in the form of this tuple (directory,[files])
